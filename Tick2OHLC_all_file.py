42ddcd66c40b99c30f5ea5fe278fe46eeb55636f
Mojtaba Abedini <108384257+mojtaba-abedini@users.noreply.github.com>
2022-12-12 09:01:16 +0330

    Update Tick2OHLC_all_file.py
    optimized ...

--- Tick2OHLC_all_file.py
+++ Tick2OHLC_all_file.py
@@ -1,26 +1,30 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[60]:
 
 
 import pandas as pd
 import os, glob
+from tqdm import tqdm
+import warnings
 
 
-# In[2]:
+# In[61]:
 
+
+warnings.filterwarnings("ignore")
 
 all_files = glob.glob("*.txt")
 li = []
 for filename in all_files:
-    df = pd.read_fwf(filename, index_col=None, header=0)
+    df = pd.read_csv(filename, index_col=None, header=0)
     li.append(df)
 
 frame = pd.concat(li, axis=0, ignore_index=True)
 
 
-# In[3]:
+# In[23]:
 
 
 outputFileName= input(' * Enter your output file name ( file type is csv) : ')
@@ -52,84 +56,66 @@ timeFrameInput = int(input(""" * Enter your time frame :
     4 : M30
     5 : H1
     6 : H4
-    7 : D1         
-
+    7 : D1   
+    
     """))
 
 timeFrame = timeframe(timeFrameInput)
 
 
-# In[4]:
+# In[20]:
 
 
-df=frame
+arr=[]
+
+for i in tqdm(range(0,len(frame)),total=len(frame),desc=' * Splitting data '):
+    arr.append(frame.iloc[i][0].split())
 
 
-
-df = df.rename({'RateDateTime\tRateBid\tRateAsk': 'A', 'Unnamed: 1': 'B'}, axis=1) 
-arr= []
-bid= []
+# In[57]:
 
 
-for i in range(len(df['A'])):
-    if ('\t' in df['A'][i]):
-        arr.append((df['A'][i].replace('\t', ' ').split(' ')))
-    else:
-        a = (df['A'][i].split(' '))
-        b = [(df['B'][i])]
-        arr.append(a+b)
-        
-           
+table  = pd.DataFrame(arr)
+table = table .rename({0: 'date', 1: 'time', 2: 'bid', 3:'ask'}, axis=1)
 
 
-# In[5]:
+# In[62]:
 
 
-table = pd.DataFrame(arr)
-table = table.rename({0: 'date', 1: 'time', 2: 'bid'}, axis=1)
-table
-
-
-# In[6]:
-
-
-table["DateTime"] = table['date'].astype(str) +" "+ table["time"]
+table["DateTime"] = table["date"].astype(str) +" "+ table["time"]
 data = table[['DateTime','bid']]
-
 data['DateTime'] = pd.to_datetime(data['DateTime'])
-
 data = data.set_index(['DateTime'])
-data
 
 
-# In[7]:
+# In[63]:
 
 
 data["bid"] = pd.to_numeric(data["bid"], downcast="float")
 #data["ask"] = pd.to_numeric(data["ask"], downcast="float")
 
 
-# In[8]:
+# In[64]:
 
 
 data_ohlc = data['bid'].resample(timeFrame).ohlc()
 
 
-# In[9]:
+# In[65]:
 
 
 data_ohlc['dateTime']=data_ohlc.index
 splitedTime=[]
 
 
-# In[10]:
+# In[66]:
 
 
 data_ohlc['date'] = pd.to_datetime(data_ohlc['dateTime']).dt.date
 data_ohlc['time'] = pd.to_datetime(data_ohlc['dateTime']).dt.time
 
 
-# In[16]:
+# In[67]:
 
 
 output = data_ohlc.reset_index()
@@ -137,13 +123,13 @@ output = output[['date','time','open','high','low','close']]
 output=output[output['open'].notna()]
 
 
-# In[17]:
+# In[49]:
 
 
 output.to_csv(outputFileName+'.csv',index=False)
 
 
-# In[18]:
+# In[50]:
 
 
 print('Finished ... !')
